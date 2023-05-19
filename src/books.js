0c3cfc70b2dd397e67f7710d8eea7a3307e5bc87
Bruce <drift2galaxy@gmail.com>
2023-05-19 15:38:29 +0000

    feat: first commit

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from './images/book-1.jpg'
+import img2 from './images/book-2.jpg'
+import img3 from './images/book-3.jpg'
+
+export const books = [
+  {
+    author: 'Piggyback',
+    title: 'The Legend of Zelda',
+    img: img1,
+    id: 1,
+  },
+  {
+    author: 'Dr. Seuss',
+    title: "Oh, the Places You'll Go!",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'Emily Henry',
+    title: 'Happy Place',
+    img: img3,
+    id: 3,
+  },
+]
